d70017294266f94fc40a5733e3b6f6f59786a81c
Кирил Илиев <kiril.g.iliev@gmail.com>
2023-11-25 00:18:16 +0200

    fixin register form

--- Client/src/components/register/Register.jsx
+++ Client/src/components/register/Register.jsx
@@ -3,13 +3,13 @@ import styles from '../register/register.module.css'
 import { useForm } from '../../hooks/useForm'
 const RegisterForm = {
     Email: 'email',
-    Password: 'password',
     Username: 'username',
+    Password: 'password',
     RepearPassword: 'rePassword'
 }
 export default function Register({onRegisterSubmitHandler}) {
 
-    const { values, onChange, onSubmit} = useForm(
+    const {formValues, onChange, onSubmit} = useForm(
         {
         [RegisterForm.Email]:'',
         [RegisterForm.Password]:'',
@@ -21,18 +21,32 @@ export default function Register({onRegisterSubmitHandler}) {
         <section className={styles['registerPage']} >
             <form className={styles['registerForm']} onSubmit={onSubmit} method="post">
                 <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name={RegisterForm.Email} placeholder="Email" value={values[RegisterForm.Email]}/>
+                <input
+                onChange={onChange}
+                type="text"
+                id="email"
+                name={RegisterForm.Email}
+                placeholder="Email"
+                value={formValues[RegisterForm.Email]}
+                />
                 <label htmlFor="username">Username:</label>
                 <input
+                    onChange={onChange}
                     type="text"
                     id="username"
                     name={RegisterForm.Username}
                     placeholder="Enter your Username"
-                    onChange={onChange}
-                    value={values[RegisterForm.Username]}
+                    value={formValues[RegisterForm.Username]}
                 />
                 <label htmlFor="password">Password:</label>
-                <input onChange={onChange} type="password" id="password" name={RegisterForm.Password} placeholder="Password" value={RegisterForm.Password} />
+                <input
+                onChange={onChange}
+                type="password"
+                id="password"
+                name={RegisterForm.Password}
+                placeholder="Password"
+                value={formValues[RegisterForm.Password]}
+                 />
                 <label htmlFor="rePassword">Repeat Password:</label>
                 <input
                 onChange={onChange}
@@ -40,9 +54,9 @@ export default function Register({onRegisterSubmitHandler}) {
                     id="rePassword"
                     name={RegisterForm.RepearPassword}
                     placeholder="Repeat Password"
-                    value={values[RegisterForm.RepearPassword]}
+                    value={formValues[RegisterForm.RepearPassword]}
                 />
-                <input type="submit" className={styles['register']} defaultValue="Register" />
+                <input type="submit" className={styles['register']} value="Register" />
                 <p><span >
                 If you don't have an account, please login <Link to="/auth/login">here</Link>
                     </span> </p>
