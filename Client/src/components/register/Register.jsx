67fa1c340f06cf7e057dbf4fccc61dedbc92c7fe
Кирил Илиев <kiril.g.iliev@gmail.com>
2023-11-24 23:59:45 +0200

    Hot fixes

--- Client/src/components/register/Register.jsx
+++ Client/src/components/register/Register.jsx
@@ -4,19 +4,24 @@ import { useForm } from '../../hooks/useForm'
 const RegisterForm = {
     Email: 'email',
     Password: 'password',
-    Username
+    Username: 'username',
+    RepearPassword: 'rePassword'
 }
-export default function Register() {
+export default function Register({onRegisterSubmitHandler}) {
 
-    const { values, onChange, onSubmit} = useForm({
-
+    const { values, onChange, onSubmit} = useForm(
+        {
+        [RegisterForm.Email]:'',
+        [RegisterForm.Password]:'',
+        [RegisterForm.Username]:'',
+        [RegisterForm.RepearPassword]:'',
     }, onRegisterSubmitHandler)
 
     return (
         <section className={styles['registerPage']} >
-            <form className={styles['registerForm']} action="#" method="">
+            <form className={styles['registerForm']} onSubmit={onSubmit} method="post">
                 <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name="" placeholder="Email" />
+                <input type="text" id="email" name={RegisterForm.Email} placeholder="Email" value={values[RegisterForm.Email]}/>
                 <label htmlFor="username">Username:</label>
                 <input
                     type="text"
@@ -24,15 +29,18 @@ export default function Register() {
                     name={RegisterForm.Username}
                     placeholder="Enter your Username"
                     onChange={onChange}
+                    value={values[RegisterForm.Username]}
                 />
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="" placeholder="Password" />
+                <input onChange={onChange} type="password" id="password" name={RegisterForm.Password} placeholder="Password" value={RegisterForm.Password} />
                 <label htmlFor="rePassword">Repeat Password:</label>
                 <input
+                onChange={onChange}
                     type="password"
                     id="rePassword"
-                    name=""
+                    name={RegisterForm.RepearPassword}
                     placeholder="Repeat Password"
+                    value={values[RegisterForm.RepearPassword]}
                 />
                 <input type="submit" className={styles['register']} defaultValue="Register" />
                 <p><span >
