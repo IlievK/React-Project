eb90c458dbbb7a4d5491bfe6db9923ae7f606435
Кирил Илиев <kiril.g.iliev@gmail.com>
2023-11-27 23:36:56 +0200

    DEtails page small fixes

--- Client/src/components/details/Details.jsx
+++ Client/src/components/details/Details.jsx
@@ -1,25 +1,25 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { getOne } from "../../api/gamesApi"
-
+import styles from '../catalog/catalog.module.css'
 export default function Details(){
 const {id} = useParams()
 const [item, setItem] = useState({})
 
 useEffect(()=>{
     getOne(id)
-    .then(respone => console.log(respone))
+    .then(respone => setItem(respone))
     .catch(error => console.log(error))
 })
     return(
         <>
-        {/* <img src={image} className={styles['picture-added-electronics']} />
-        <p>{name}</p>
-        <p>{type}</p>
-        <p>{production}</p>
-        <p>{price}</p>
-        <p>{description}</p> */}
-        <div>Details</div>
+        <img src={item.image} className={styles['picture-added-electronics']} />
+        <p>{item.name}</p>
+        <p>{item.type}</p>
+        <p>{item.production}</p>
+        <p>{item.price}</p>
+        <p>{item.description}</p>
+        
         
         </>
     )
