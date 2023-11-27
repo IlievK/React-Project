592341b30d8bc5c323fc66253b4f11d62d5dad87
Кирил Илиев <kiril.g.iliev@gmail.com>
2023-11-27 20:23:38 +0200

    API update

--- Client/src/utils/api.js
+++ Client/src/utils/api.js
@@ -26,6 +26,10 @@ async function request(method, url, data) {
       const error = await response.json();
       throw new Error(error.message);
     }
+    // TO DO - to be checked if it's working for logout!
+    if(response.status === 204){
+      return {}
+    }
 
     return await response.json();
   } catch (error) {
