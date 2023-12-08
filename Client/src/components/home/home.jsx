91a5e4f31964e2311c606458c39d8316ab74bca2
Кирил Илиев <kiril.g.iliev@gmail.com>
2023-12-08 19:23:04 +0200

    Home update

--- Client/src/components/home/home.jsx
+++ Client/src/components/home/home.jsx
@@ -2,10 +2,10 @@ import styles from '../home/home.module.css'
 export default function Home(){
     return (
         
-        <section id="homePage" className={styles['background-img']}>
+        <section id={styles['homePage']} className={styles['background-img']}>
             <div className={styles['introduction']}>
-                <h1>The Trusted Wholesale Marketplace for Used Electronics</h1>
-                <p>Each year, 50 million tons of IT gear reaches end-of-life. Tradeloop is where companies safely buy, repair, recycle, and resell it. How it works?</p>
+                <h1>React Project Home page</h1>
+                <p>Hello react</p>
             </div>
         </section>
     )
